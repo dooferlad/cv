7305b08588f34ef8166687abf547f9577e6709e4
James Tunnicliffe <james.tunnicliffe@nanosheep.org>
2014-10-30 15:26:21 +0000

    Removed non-functional docker bits

--- Docker/build.sh
+++
@@ -1,3 +0,0 @@
-docker stop $(docker ps | grep eat_the_smoke:latest | cut -f 1 -d ' ')
-shred -s10 - > random
-docker build -t eat_the_smoke . && docker run --dns=192.168.0.2 -p 80:80 -t eat_the_smoke
